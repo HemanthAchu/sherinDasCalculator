af4e35cf0112dbea6f6c7da04e6981eed6df24e5
HemanthAchu <hemanthms186@gmail.com>
2023-12-24 11:17:18 +0530

    ReactQuiz

--- src/App.jsx
+++ src/App.jsx
@@ -1,73 +1,96 @@
 
-import { useState } from 'react'
-
+import { data } from './data'
 import './App.css'
-
-
-
-
-
-
-
+import { useState } from 'react'
+import { useRef } from 'react'
 
 function App() {
+const [index,setindex]=useState(0)
+const [question,setquestion]=useState(data[index])
+const [lock, setlock]=useState(false)
+const [score ,setscore] =useState(0)
+const[result,setresult] =useState(false)
 
-  const [input,setInput]=useState('')
-  const [result,setResult]=useState('')
-  const clicked=(value)=>{
-    if (value === '=') {
-      try {
-        setResult(eval(input));
-       setInput('')
-      } catch (error) {
-        setResult('Error');
-      }
-    } 
-    else if (value === 'Ac') {
-      setInput('');
-      setResult('');
-    }
-    else if(value=='clear'){
-      setInput((value)=>value.slice(0,-1))
-    }
-     else {
-      setInput((Inputs) => Inputs + value);
-    }
+let option1=useRef(null)
+let option2=useRef(null)
+let option3=useRef(null)
+let option4=useRef(null)
+const good =document.getElementById('good')
+let option_array =[option1,option2,option3,option4]
+
+const checkAns =(e,ans)=>{
+if(lock===false){
+  if(question.ans===ans){
+    e.target.classList.add("win")
+    setlock(true)
+    setscore(score+1)
+  }else{
+    e.target.classList.add("fail")
+    setlock(true)
+    option_array[question.ans-1].current.classList.add("win");
   }
+}
+}
 
 
+
+
+      const Nextbtn=(e)=>{
+      if (lock===true) {
+        if(index+1===data.length){
+          setresult(true)
+          return 0;
+          }
+        setindex(index+1)
+      setquestion(data[index+1])
+      setlock(false)
+     
+      option_array.map((option)=>{
+        option.current.classList.remove("win")
+        option.current.classList.remove("fail")
+        return null
+      })
+      }
+
+
+  }
+  const reset =()=>{
+    setindex(0)
+    setlock(false)
+    setresult(false)
+    setscore(0)
+    setquestion(data[index])
+  }
+  
   return (
-    <>
-    <div className='calculator'>
-    <div className='main'>
-    <div className="main1">
-    <div className='output'>
-        <div className='previous'>{input}</div>
-        <div className='current'>{result}</div>
-  </div>  
-<button onClick={()=>clicked('Ac')} className='span-two'>Ac</button>
-<button onClick={()=>clicked('clear')} >Del</button>
-<button onClick={()=>clicked('/')}>/</button>
-<button onClick={()=>clicked('7')}>7</button>
-<button onClick={()=>clicked('8')}>8</button>
-<button onClick={()=>clicked('9')}>9</button>
-<button onClick={()=>clicked('*')}>*</button>
-<button onClick={()=>clicked('4')}>4</button>
-<button onClick={()=>clicked('5')}>5</button>
-<button onClick={()=>clicked('6')}>6</button>
-<button onClick={()=>clicked('+')}>+</button>
-<button onClick={()=>clicked('1')}>1</button>
-<button onClick={()=>clicked('2')}>2</button>
-<button onClick={()=>clicked('3')}>3</button>
-<button onClick={()=>clicked('-')}>-</button>
-<button onClick={()=>clicked('0')}>0</button>
-<button onClick={()=>clicked('.')}>.</button>
-<button onClick={()=>clicked('=')} className='span-two'>=</button>
-</div>
-</div>
+        <>
+       
+      <div className="main">
+        <div className="class">
+         <div className='class1'> <h3 id='good'>Score : {score}</h3> <h2>{index+1}/{data.length}Qus</h2>     </div>
+          {
+            result?<></>:<><h3 className='question'>{index+1}.{question.question}</h3>
+            <ul>
+              <li onClick={(e)=>checkAns(e,1)} ref={option1}>{question.option1}</li>
+              <li onClick={(e)=>checkAns(e,2)}  ref={option2}>{question.option2}</li>
+              <li onClick={(e)=>checkAns(e,3)} ref={option3}>{question.option3}</li>
+              <li  onClick={(e)=>checkAns(e,4)} ref={option4}>{question.option4}</li>
+            </ul>
+   <center><button onClick={(e)=>Nextbtn(e)} className=' btn  w-75'>Next</button></center>
+  </>
+          }
 
-    </div>
-    </>
+{
+  result?<><center><button onClick={reset} className='btn'>Reset</button></center></>:<></>
+}
+
+
+
+
+        </div>
+      </div>
+        </>
+          
   )
 }
 
